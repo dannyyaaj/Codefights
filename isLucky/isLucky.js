dd309e5b8e5b005475dc9e20e8e5f853fc2d2c31
Danny Yang <dannyyaaj@gmail.com>
2018-03-16 17:08:43 -0500

    Added isLucky algorithm

---
+++ isLucky/isLucky.js
@@ -0,0 +1,14 @@
+function isLucky(n) {
+n = n.toString();
+let half = n.length/2;
+
+let firstHalf = n.substring(0, half).split('').reduce((a,b) => {
+  return parseInt(a) + parseInt(b);
+});
+
+let secondHalf = n.substring(half, n.length).split('').reduce((a,b) => {
+  return parseInt(a) + parseInt(b);
+});
+
+  return firstHalf === secondHalf;
+}
